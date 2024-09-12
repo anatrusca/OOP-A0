95ad501cbcec8e3ec07d40ff7584811ec808ee4f
anatrusca <a.trusca@student.rug.nl>
2024-09-12 18:57:14 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Create a list containing all numbers from 1 to 10
+numbers = list(range(1, 11))
+# Create a numpy array from this list
+array = np.array(numbers)
+# Print the numpy array
+print(array)
